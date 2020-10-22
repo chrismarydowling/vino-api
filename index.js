cf883245da58372d9508dab8384db899964beaeb
Ren <rsapaitis@gmail.com>
2020-10-22 22:26:09 +0100

    added /bottles endpoint.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Do the import thing! As a point of interest express 
+// Do the import thing! As a point of interest express
 // exports a function
 const express = require('express');
 const mongoose = require('mongoose');
@@ -7,11 +7,44 @@ const port = 4545;
 
 let app = express();
 
-app.get('/hello', (req, res) => {
-    res.send('hello world');
+let bottleSchema = new mongoose.Schema({
+  name: String,
+  country: String,
+  colour: String,
+  year: Number,
+  grape: String
+});
+
+let Bottle = mongoose.model('Bottle', bottleSchema);
+
+
+
+
+app.get('/bottles', (req, res) => {
+  Bottle.find((err, bottles)=>{
+    if(err) {
+      console.log(err);
+    }
+    console.log(bottles[0]);
+    res.send(JSON.stringify(bottles));
+  });
+
 });
 
 function start() {
+
+  let bottle1 = new Bottle ({
+    name: "test bottle",
+    country: "germany",
+    colour: "red",
+    year: 2012,
+    grape: "pinot noir"
+  });
+
+  // bottle1.save(() =>  {
+  //  console.log("saved the bottle?")
+  //});
+
     app.listen(port, () => {
         console.log("Server is up and running on " + port + "!");
     });
@@ -22,3 +55,30 @@ mongoose.connect("mongodb://localhost/vino", { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'))
 db.once('open', start);
+
+
+
+
+[
+
+      {
+         name: "test bottle",
+         country: "germany",
+         colour: "red",
+         year: 2012,
+         grape: "pinot noir"
+      },
+      {
+         name: "test bottle2",
+         country: "germany",
+         colour: "red",
+         year: 2012,
+         grape: "pinot noir"
+      },
+      {
+         name: "test bottle3",
+         country: "germany",
+         colour: "red",
+         year: 2012,
+         grape: "pinot noir"
+      }]
